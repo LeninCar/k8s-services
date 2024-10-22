e8102ee698a30939650743caf5b3a1c155d5adfc
LeninCar <lenin.carabali@correounivalle.edu.co>
2024-10-22 18:26:05 -0500

    fisrt commit

---
+++ apigw/apigw.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+# Endpoint en Servicio Orquetador para iniciar la orquestación
+@app.get("/orquestar")
+async def orquestar():
+    async with httpx.AsyncClient() as client:
+        try:
+            respuesta_a = await client.get("http://servicio-a-service/serviciolenin")
+            data_a = respuesta_a.json()
+        except httpx.RequestError:
+            data_a = "El servicio de Lenin no está disponible"
+
+    return {"respuesta Lenin": data_a}
